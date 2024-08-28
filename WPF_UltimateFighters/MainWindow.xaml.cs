b11abb46e360ff79d36b392ea32d0b03c26fd8e5
giolga <giolga.kumi@gmail.com>
2024-08-28 17:14:14 +0400

    First commit

---
+++ WPF_UltimateFighters/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_UltimateFighters
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        SqlConnection sqlConnection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+
+            ShowDivisions();
+        }
+
+        private void ShowDivisions()
+        {
+            try
+            {
+                string query = @"SELECT * FROM WeightClass";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (adapter)
+                {
+                    DataTable divisionTable = new DataTable();
+                    adapter.Fill(divisionTable);
+
+                    ListDivisions.DisplayMemberPath = "weight-class";
+                    ListDivisions.SelectedValuePath = "Id";
+                    ListDivisions.ItemsSource = divisionTable.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
